11d3fb1afd38548d6d9257bf0c1bda02595a6d05
Brandi Murray <pritchettb14@gmail.com>
2021-08-04 19:22:42 -0400

    added dark/light theme toggle option

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './style.css'
+import styled from 'styled-components'
+
 
 function Intro() {
     return (
         <div>
-         <h1> Hello, <br/>
+         <h1 className="intro"> Hello, <br/>
          My name is Brandi</h1>
          <br/><br/>
          I like to code. [design, game, learn, party]
